c1da2bb5038ce7f82d178ad3265c88b08baab203
Sameera <basameera@protonmail.com>
2022-08-11 17:16:12 +0200

    update

--- complex_plugin/complex_plugin_action.py
+++ complex_plugin/complex_plugin_action.py
@@ -1,7 +1,9 @@
 import pcbnew
 import os
-import datetime
 import re
+import datetime
+import wx
+
 
 class ComplexPluginAction(pcbnew.ActionPlugin):
     def defaults(self):
@@ -13,10 +15,76 @@ class ComplexPluginAction(pcbnew.ActionPlugin):
             os.path.dirname(__file__), 'icon.png')  # Optional
 
     def Run(self):
-        pcb = pcbnew.GetBoard()
-        for draw in pcb.GetDrawings():
-            if draw.GetClass() == 'PTEXT':
-                txt = re.sub("\$date\$ [0-9]{4}-[0-9]{2}-[0-9]{2}",
-                             "$date$", draw.GetText())
-                if txt == "$date$":
-                    draw.SetText("%s" % datetime.date.today())
+        '''
+        Replace a text containing $date$ with today's date.
+        '''
+        # pcb = pcbnew.GetBoard()
+        # for draw in pcb.GetDrawings():
+        #     if draw.GetClass() == 'PTEXT':
+        #         txt = re.sub("\$date\$ [0-9]{4}-[0-9]{2}-[0-9]{2}",
+        #                      "$date$", draw.GetText())
+        #         if txt == "$date$":
+        #             draw.SetText("%s" % datetime.date.today())
+
+        start_GUI2()
+
+
+def start_GUI2():
+    app = wx.App(False)
+    frame = MyFrame(None, 'Small editor')
+    app.MainLoop()
+
+
+class MyFrame(wx.Frame):
+    """ We simply derive a new class of Frame. """
+    '''https://docs.wxwidgets.org/2.8.12/wx_stdevtid.html'''
+
+    def __init__(self, parent, title):
+        height = 800
+        width = 1000
+        height_half = height//2
+        width_half = width//2
+
+        wx.Frame.__init__(self, parent, title=title, size=(width, height))
+
+        # add panel
+        self.panel = wx.Panel(self, wx.ID_ANY, pos=(0, 0),
+                              size=(width_half, height), style=wx.SIMPLE_BORDER)
+        panel1 = wx.Panel(self, wx.ID_ANY, pos=(width_half, 0),
+                          size=(width_half, height), style=wx.SIMPLE_BORDER)
+
+        # add button
+        button = wx.Button(self.panel, wx.ID_ANY, 'About', (10, 200))
+        button.Bind(wx.EVT_BUTTON, self.OnAbout)
+
+        button1 = wx.Button(panel1, wx.ID_ANY, 'Exit', (10, 10))
+        button1.Bind(wx.EVT_BUTTON, self.OnExit)
+
+        # text
+        self.output = wx.StaticText(self.panel, label="Output:", pos=(10, 10),
+                                    size=(100, 20), style=wx.SIMPLE_BORDER)
+        self.result = wx.StaticText(self.panel, label="--output--", pos=(110, 10),
+                                    size=(100, 20), style=wx.SIMPLE_BORDER)
+        self.result.SetForegroundColour(wx.RED)
+
+        self.input = wx.StaticText(self.panel, label="Input:", pos=(10, 40),
+                                   size=(100, 20), style=wx.SIMPLE_BORDER)
+        self.editname = wx.TextCtrl(self.panel, pos=(110, 40),
+                                    size=(140, -1))
+        self.button = wx.Button(self.panel, label="Save", pos=(10, 70))
+        self.button.Bind(wx.EVT_BUTTON, self.OnButton)
+
+        self.Show(True)
+
+    def OnAbout(self, event):
+        wx.LogMessage("on about")
+        # if (some_condition):
+        #     do_something()
+        # else:
+        #     event.Skip()
+
+    def OnExit(self, e):
+        self.Close(True)  # Close the frame.
+
+    def OnButton(self, e):
+        self.result.SetLabel(self.editname.GetValue())
